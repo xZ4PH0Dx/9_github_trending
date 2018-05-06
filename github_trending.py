476b71b702c2cacc1a0c88a1aca22045d907abff
Z4PH0D <dreamfall3r@gmail.com>
2018-05-06 02:51:46 +0200

    First version of a program

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,26 @@
+import requests
+from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_maxstars_items(url, days_to_substract=7, items_per_page=20):
+    begin_date = date.today() - timedelta(days_to_substract)
+    payload = ('q=language:python+created:>{}'
+               '&per_page={}&sort=stars&order=desc'
+               .format(begin_date, items_per_page))
+    return requests.get(url, params=payload).json()['items']
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_repo_stats(maxstars_items):
+    return (maxstars_items['html_url'],
+            maxstars_items['stargazers_count'],
+            maxstars_items['open_issues_count']
+            )
+
 
 if __name__ == '__main__':
-    pass
+    url = 'https://api.github.com/search/repositories'
+    maxstars_items_list = get_maxstars_items(url)
+    print('A list of new github repos with max stargazers count:\n')
+    for item in maxstars_items_list:
+        html_url, stargazers_count, open_issues = get_repo_stats(item)
+        print(html_url, 'stars:', stargazers_count, 'open issues:', open_issues)
