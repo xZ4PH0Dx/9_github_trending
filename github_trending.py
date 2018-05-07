1b2bea4e0a61b1a785955a697938367bf6c1fa1c
Z4PH0D <dreamfall3r@gmail.com>
2018-05-07 12:18:52 +0200

    changed the way getting issues_list

--- github_trending.py
+++ github_trending.py
@@ -2,25 +2,35 @@ import requests
 from datetime import date, timedelta
 
 
-def get_maxstars_items(url, days_to_substract=7, items_per_page=20):
+def get_maxstars_items(url, days_to_substract=7, top_repos=20):
     begin_date = date.today() - timedelta(days_to_substract)
-    payload = ('q=language:python+created:>{}'
-               '&per_page={}&sort=stars&order=desc'
-               .format(begin_date, items_per_page))
-    return requests.get(url, params=payload).json()['items']
+    payload = {
+            'sort': 'stars',
+            'order': 'desc',
+            'q': 'created:>{}'
+            .format(begin_date)
+            }
+    return requests.get(url, params=payload).json()['items'][:top_repos]
 
 
-def get_repo_stats(maxstars_items):
-    return (maxstars_items['html_url'],
-            maxstars_items['stargazers_count'],
-            maxstars_items['open_issues_count']
+def get_repo_stars(repo):
+    return (
+            repo['html_url'],
+            repo['stargazers_count'],
             )
 
 
+def get_count_issues(owner, repo_name):
+    url = 'https://api.github.com/repos/{}/{}/issues'.format(owner, repo_name)
+    return len(requests.get(url).json())
+
+
 if __name__ == '__main__':
-    url = 'https://api.github.com/search/repositories'
-    maxstars_items_list = get_maxstars_items(url)
+    repos_url = 'https://api.github.com/search/repositories'
+    maxstars_repos_list = get_maxstars_items(repos_url)
     print('A list of new github repos with max stargazers count:\n')
-    for maxstars_item in maxstars_items_list:
-        html_url, stargazers, open_issues = get_repo_stats(maxstars_item)
+    for repo in maxstars_repos_list:
+        html_url, stargazers = get_repo_stars(repo)
+        owner, repo_name = repo['full_name'].split('/')
+        open_issues = get_count_issues(owner, repo_name)
         print(html_url, 'stars:', stargazers, 'issues:', open_issues)
